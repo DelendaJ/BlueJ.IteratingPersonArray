59fcf7a578b57d304a8157b4f3043ea581c7038b
Delenda Joseph <delendajoseph@zipcoders-MacBook-Pro.local>
2019-02-10 14:13:12 -0500

    iterated arrays

--- PersonHandler.java
+++ PersonHandler.java
@@ -1,4 +1,3 @@
-package com.zipcodewilmington;
 
 /**
  * Created by leon on 1/24/18.
@@ -12,52 +11,40 @@ public class PersonHandler {
 
     public String whileLoop() {
         String result = "";
-        // assume there is a `counter`
-        // while `counter` is less than length of array
-            // begin loop
+        int counter = 0;
+        Person currentPerson;
+        while (counter < personArray.length) {
+            currentPerson = personArray[counter];
+            String stringRepresentation = currentPerson.toString();
+            result += stringRepresentation; 
+            counter++;
+        }
 
-                // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
-
-            // end loop
         return result;
     }
-
-
 
     public String forLoop() {
         String result = "";
-        // identify initial value
-        // identify terminal condition
-        // identify increment
+        Person currentPerson;
+        for (int counter = 0; counter < personArray.length; counter++){
+            currentPerson = personArray[counter];
+            String stringRepresentation = currentPerson.toString();
+            result += stringRepresentation;
 
-        // use the above clauses to declare for-loop signature
-            // begin loop
-                // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
-            // end loop
+        }
 
         return result;
     }
-
-
 
     public String forEachLoop() {
         String result = "";
-        // identify array's type
-        // identify array's variable-name
-
-        // use the above discoveries to declare for-each-loop signature
-            // begin loop
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
-            // end loop
+        for (Person currentPerson : personArray) {
+            String stringRepresentation = currentPerson.toString();
+            result += stringRepresentation;
+        }
 
         return result;
     }
-
 
     public Person[] getPersonArray() {
         return personArray;
